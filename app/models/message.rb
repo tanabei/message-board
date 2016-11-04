092e91921f9c33d12689f9d52e238a8a85a5c1ba
tanabe <nabeta4@gmail.com>
2016-11-04 16:54:52 +0000

    message board 課題2

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,7 @@ class Message < ActiveRecord::Base
     validates :name,length:{maximum:20},presence:true
     #内容必須入力かつ2文字以内以上以上３０文字以下
     validates :body,length:{minimum:2,maximum:30},presence:true
+    #年齢必須入力かつ0以上の数字だけ入力可能
+    validates :age,numericality: {only_integer: true, greater_than:1},presence:true
 end
 
